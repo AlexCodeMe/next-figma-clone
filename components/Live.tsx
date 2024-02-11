40d0c77d121304da8553885d5e2809f755503960
alexmoyer <chloegome@icloud.com>
2024-02-11 12:56:19 -0500

    CursorChat working

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,10 +1,16 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import LiveCursors from './cursor/LiveCursors'
 import { useMyPresence, useOthers } from '@/liveblocks.config'
+import CursorChat from './cursor/CursorChat'
+import { CursorMode } from '@/types/type'
 
 const Live = () => {
   const others = useOthers()
   const [{ cursor }, updateMyPresence] = useMyPresence() as any
+
+  const [cursorState, setCursorState] = useState({
+    mode: CursorMode.Hidden,
+  })
 
   const handlePointerMove = useCallback((event: React.PointerEvent) => {
     event.preventDefault()
@@ -16,8 +22,7 @@ const Live = () => {
   }, [])
 
   const handlePointerLeave = useCallback((event: React.PointerEvent) => {
-    event.preventDefault()
-
+    setCursorState({ mode: CursorMode.Hidden })
     updateMyPresence({ cursor: null, message: null })
   }, [])
 
@@ -28,6 +33,35 @@ const Live = () => {
     updateMyPresence({ cursor: { x, y } })
   }, [])
 
+  useEffect(() => {
+    const onKeyUp = (e: KeyboardEvent) => {
+      if (e.key === '/') {
+        setCursorState({
+          mode: CursorMode.Chat,
+          previousMessage: null,
+          message: '',
+        })
+      } else if (e.key === 'Escape') {
+        updateMyPresence({ message: ''})
+        setCursorState({ mode: CursorMode.Hidden })
+      }
+    }
+
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === '/') {
+        e.preventDefault()
+      }
+    }
+
+    window.addEventListener('keyup', onKeyUp)
+    window.addEventListener('keydown', onKeyDown)
+
+    return () => {
+      window.addEventListener('keyup', onKeyUp)
+      window.addEventListener('keydown', onKeyDown)
+    }
+  }, [updateMyPresence])
+
   return (
     <div
       onPointerMove={handlePointerMove}
@@ -36,6 +70,15 @@ const Live = () => {
       className='h-[100vh] w-full flex justify-center items-center'
     >
       <h1 className='text-5xl text-white'>FigPro by Alexander</h1>
+
+      {cursor && (
+        <CursorChat 
+          cursor={cursor}
+          cursorState={cursorState}
+          setCursorState={setCursorState}
+          updateMyPresence={updateMyPresence}
+        />
+      )}
 
       <LiveCursors others={others} />
     </div>

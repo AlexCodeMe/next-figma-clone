66e99d8f9560c47f489dc3b0b48485f5a6c51471
alexmoyer <chloegome@icloud.com>
2024-02-11 12:20:22 -0500

    initial

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Work_Sans } from "next/font/google";
+import "./globals.css";
+import { Room } from "./Room";
+
+const work_sans = Work_Sans({ 
+  subsets: ["latin"],
+  variable: '--font-work-sans',
+  weight: ['400','600','700']
+});
+
+export const metadata: Metadata = {
+  title: "Figma Clone",
+  description: "Figma clone using Fabric.js and Liveblocks",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${work_sans.className} bg-primary-grey-200`}>
+        <Room>
+          {children}
+        </Room>
+      </body>
+    </html>
+  );
+}
